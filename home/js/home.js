c11edc818df7987f674f544262b021efcfd73e06
zhao.erica/赵俊_京_校园招聘 <erica.zhao@yu1.51job.com>
2018-08-28 14:00:47 +0800

    14:00

--- home/js/home.js
+++ home/js/home.js
@@ -169,40 +169,6 @@ window.onload = function () {
   mapInit();
 }
 
-// main_kv 跟随鼠标提示
-var tip = {
-  $: function (ele) {
-    if (typeof (ele) == "object")
-      return ele;
-    else if (typeof (ele) == "string" || typeof (ele) == "number")
-      return document.getElementById(ele.toString());
-    return null;
-  },
-  mousePos: function (e) {
-    var x, y;
-    var e = e || window.event;
-    return {
-      x: e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
-      y: e.clientY + document.body.scrollTop + document.documentElement.scrollTop
-    };
-  },
-  start: function (obj) {
-    var self = this;
-    var t = self.$("mjs:tip");
-    obj.onmousemove = function (e) {
-      var mouse = self.mousePos(e);
-      t.style.left = mouse.x + 10 + 'px';
-      t.style.top = mouse.y + 10 + 'px';
-      t.innerHTML = obj.getAttribute("tips");
-      t.style.display = '';
-    };
-    obj.onmouseout = function () {
-      t.style.display = 'none';
-    };
-  }
-}
-
-
 // (function () {
 //   // swiper
 //   var mySwiper = new Swiper('.swiper-container', {
@@ -342,10 +308,41 @@ window.onhashchange = classInit = function () {
       index = 0;
       changeClass(index);
   }
-
 };
 classInit();
 
+// main_kv鼠标跟随文字提示
+var tip = {
+  $: function (ele) {
+    if (typeof (ele) == "object")
+      return ele;
+    else if (typeof (ele) == "string" || typeof (ele) == "number")
+      return document.getElementById(ele.toString());
+    return null;
+  },
+  mousePos: function (e) {
+    var x, y;
+    var e = e || window.event;
+    return {
+      x: e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+      y: e.clientY + document.body.scrollTop + document.documentElement.scrollTop
+    };
+  },
+  start: function (obj) {
+    var self = this;
+    var t = self.$("mjs:tip");
+    obj.onmousemove = function (e) {
+      var mouse = self.mousePos(e);
+      t.style.left = mouse.x + 20 + 'px';
+      t.style.top = mouse.y + 0 + 'px';
+      t.innerHTML = obj.getAttribute("tips");
+      t.style.display = '';
+    };
+    obj.onmouseout = function () {
+      t.style.display = 'none';
+    };
+  }
+}
 
 /* var $active = $nav_list.find('.active');
 
